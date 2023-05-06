79492924c8c61a1b1c47737e7c71fdd5744814da
hyper <hyperskill@hyperskill.org>
2023-05-06 17:54:16 -0600

    Added views, added corresponding urls, fixed static files handling in base.html, index.html now uses a template base.html

---
+++ MevetProject/rental_ads/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('kino/', views.kino, name='kino'),
+    path('kon/', views.kon, name='kon'),
+    path('pagefoto/', views.pagefoto, name='pagefoto'),
+    path('svadba/', views.svadba, name='svadba'),
+]
--- MevetProject/rental_ads/views.py
+++ MevetProject/rental_ads/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def kino(request):
+    return render(request, 'kino.html')
+
+def kon(request):
+    return render(request, 'kon.html')
+
+def pagefoto(request):
+    return render(request, 'pagefoto.html')
+
+def svadba(request):
+    return render(request, 'svadba.html')
