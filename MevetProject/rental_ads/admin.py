972c0234941302bb1dce76bba6accb33b8efbd12
hyper <hyperskill@hyperskill.org>
2023-05-08 17:11:47 -0600

    Added rental_detail view, now id is hown for rentals in admin panel, added primitive rental_detail.html

--- MevetProject/rental_ads/admin.py
+++ MevetProject/rental_ads/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Category, Rental, RentalImage, Contact
+from .models import Rental, Category, RentalImage, Contact  # Add other models as needed
 
 admin.site.register(Category)
-admin.site.register(Rental)
 admin.site.register(RentalImage)
 admin.site.register(Contact)
+
+
+class RentalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'address', 'price', 'contact', 'created_at', 'updated_at')
+
+admin.site.register(Rental, RentalAdmin)
