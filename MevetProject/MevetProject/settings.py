7b26db8ca4a93413eb750af302838d3f3f311640
Mixysami <mixysami@MacBook-Pro-Mixysami.local>
2023-05-08 21:03:41 +0300

    Adding admin, database, and dice with a for loop

--- MevetProject/MevetProject/settings.py
+++ MevetProject/MevetProject/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- MevetProject/rental_ads/models.py
+++ MevetProject/rental_ads/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Rental(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    address = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    contact = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='rental_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
