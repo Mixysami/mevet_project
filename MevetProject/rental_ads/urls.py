af78a52f213477e2f343e1764bc904210cf7d54f
hyper <hyperskill@hyperskill.org>
2023-05-09 13:37:22 -0600

    Added simple templates 'login.html' and 'register.html', added corresponding views and urls

--- MevetProject/rental_ads/urls.py
+++ MevetProject/rental_ads/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
     path('kino/', views.kino, name='kino'),
     path('kon/', views.kon, name='kon'),
     path('pagefoto/', views.pagefoto, name='pagefoto'),
--- MevetProject/rental_ads/views.py
+++ MevetProject/rental_ads/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
 from .models import Rental, Category, RentalImage
 
 def index(request):
     return render(request, 'index.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse('Invalid credentials')
+    else:
+        return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
 
 def kino(request):
     kino_category = Category.objects.get(name='kino')
