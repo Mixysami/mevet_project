3d0f4878e09314bc9104b217693806cc6ff519b1
mixysami <bochkovoy9@gmail.com>
2023-05-12 01:38:14 +0300

    Registration Works

---
+++ MevetProject/rental_ads/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from models import *
+
+# class UserCreationForm(UserCreationForm):
+#     class Meta(UserCreationForm.Meta):
+#         model = User
+#         fields = ('username', 'password1', 'password2")
+#         widgets = {
+#             'username': forms. TextInput (attrs={'class': 'form-input'}),
+#             'password1': forms. PasswordInput(attrs={'class': 'form-input'}),
+#             'password2': forms. PasswordInput(attrs={'class': 'form-input'}),
+#         }
