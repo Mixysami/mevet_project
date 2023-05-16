43af2079c33b1c966909bbe73a22321aa8e4b915
mixysami <bochkovoy9@gmail.com>
2023-05-16 20:59:12 +0300

    Save Process redisign forms add_rental and edit_rental

--- MevetProject/rental_ads/forms.py
+++ MevetProject/rental_ads/forms.py
@@ -6,4 +6,12 @@ class RentalForm(forms.ModelForm):
     class Meta:
         model = Rental
         fields = ['category', 'title', 'description', 'address', 'price', 'contact', 'main_image']
-        widgets = {'category': forms.Select(choices=Category.objects.all())}
+        widgets = {
+          'category': forms.Select(choices=Category.objects.all(), attrs={'class': 'form-control'}),
+          'title': forms.TextInput(attrs={'class': 'form-control'}),
+          'description': forms.Textarea(attrs={'class': 'form-control'}),
+          'address': forms.TextInput(attrs={'class': 'form-control'}),
+          'price': forms.NumberInput(attrs={'class': 'form-control'}),
+          'contact': forms.TextInput(attrs={'class': 'form-control'}),
+          'main_image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+        }
