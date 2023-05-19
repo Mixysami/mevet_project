378b1ef70bc8b0d06cece8512a8507f154b42fb2
mixysami <bochkovoy9@gmail.com>
2023-05-19 16:23:41 +0300

    Незаконченный дизай редактирования но все работает начинаю от удаления заканчивая добавлением

--- MevetProject/rental_ads/forms.py
+++ MevetProject/rental_ads/forms.py
@@ -1,17 +1,30 @@
 from django import forms
-from .models import Rental, Category
+from .models import Rental, Category, RentalImage
 
+from multiupload.fields import MultiFileField
 
 class RentalForm(forms.ModelForm):
+    images = MultiFileField(min_num=1, max_num=10, max_file_size=1024*1024*5, required=False)
+
     class Meta:
         model = Rental
-        fields = ['category', 'title', 'description', 'address', 'price', 'contact', 'main_image']
+        fields = ['category', 'title', 'description', 'address', 'price', 'contact', 'main_image', 'images']
         widgets = {
-          'category': forms.Select(choices=Category.objects.all(), attrs={'class': 'form-control'}),
-          'title': forms.TextInput(attrs={'class': 'form-control'}),
-          'description': forms.Textarea(attrs={'class': 'form-control'}),
-          'address': forms.TextInput(attrs={'class': 'form-control'}),
-          'price': forms.NumberInput(attrs={'class': 'form-control'}),
-          'contact': forms.TextInput(attrs={'class': 'form-control'}),
-          'main_image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+            'category': forms.Select(choices=Category.objects.all(), attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'contact': forms.TextInput(attrs={'class': 'form-control'}),
+            'main_image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
         }
+
+        def clean_images(self):
+            images = self.cleaned_data.get('images', [])
+            instance = self.instance
+            if instance.pk:  # Если редактируется существующее объявление
+                existing_images = instance.images.all()
+                for image in existing_images:
+                    if image.image not in images:
+                        image.delete()
+            return images
